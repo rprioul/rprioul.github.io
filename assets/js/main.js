6d723ae5ef75895788d7b2122e3a492a8a7e3f0b
rprioul <remy.prioul@gmail.com>
2017-11-15 11:42:54 +0100

    fix main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
-jQuery(document).ready(function($) {
-    $('.level-bar-inner').css('width', '0');    
-    $(window).on('load', function() {
+jQuery(document).ready(($) => {
+    $('.level-bar-inner').css('width', '0');
+    $(window).on('load', () => {
         $('.level-bar-inner').each(function() {
-            var itemWidth = $(this).data('level');
-            $(this).animate({}, 800);
+            let itemWidth = $(this).data('level');
+            $(this).animate({
+                width: itemWidth
+            }, 800);
         });
     });
 });
